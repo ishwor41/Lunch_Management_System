f3039ae56956597373961b3c92408499c6bacc4a
Ishwor Pudasaini <ishwor.bisage@gmail.com>
2026-01-26 21:48:13 +0545

    Added UI for different menus

--- Database_Applcation/Form3.Designer.cs
+++ Database_Applcation/Form3.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            employeeBtn = new Button();
+            lunchBtn = new Button();
+            setLunch = new Button();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // employeeBtn
+            // 
+            employeeBtn.Location = new Point(98, 90);
+            employeeBtn.Name = "employeeBtn";
+            employeeBtn.Size = new Size(177, 177);
+            employeeBtn.TabIndex = 0;
+            employeeBtn.Text = "Employee";
+            employeeBtn.UseVisualStyleBackColor = true;
+            employeeBtn.Click += button1_Click;
+            // 
+            // lunchBtn
+            // 
+            lunchBtn.Location = new Point(353, 121);
+            lunchBtn.Name = "lunchBtn";
+            lunchBtn.Size = new Size(177, 177);
+            lunchBtn.TabIndex = 0;
+            lunchBtn.Text = "Lunch";
+            lunchBtn.UseVisualStyleBackColor = true;
+            lunchBtn.Click += button1_Click;
+            // 
+            // setLunch
+            // 
+            setLunch.Location = new Point(602, 90);
+            setLunch.Name = "setLunch";
+            setLunch.Size = new Size(177, 177);
+            setLunch.TabIndex = 0;
+            setLunch.Text = "Set Lunch";
+            setLunch.UseVisualStyleBackColor = true;
+            setLunch.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(employeeBtn);
+            panel1.Controls.Add(setLunch);
+            panel1.Location = new Point(12, 31);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(992, 396);
+            panel1.TabIndex = 1;
             // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1028, 450);
+            Controls.Add(lunchBtn);
+            Controls.Add(panel1);
             Name = "Form3";
             Text = "Form3";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button employeeBtn;
+        private Button lunchBtn;
+        private Button setLunch;
+        private Panel panel1;
     }
 }
